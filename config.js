aaaec2a445a285a75cbf0161b64441e687f651a9
Fabian Ardila <fabianra97@gmail.com>
2020-03-27 12:55:17 -0500

    Configure DB

--- config.js
+++ config.js
@@ -4,5 +4,11 @@ module.exports = {
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'TODOsecret!',
+    },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'remotemysql.com',
+        user: process.env.MYSQL_USER || 'SwsEC1Z1ZO',
+        password: process.env.MYSQL_PASS || 'r7KjqfRjgn',
+        database: process.env.MYSQL_DB || 'SwsEC1Z1ZO',
     }
 }
---
+++ store/mysql.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql');
+
+const config = require('../config');
+
+const dbconf = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database
+};
+
+// Connection
+let connection;
+
+function handleCon() {
+    connection = mysql.createConnection(dbconf);
+
+    connection.connect(err => {
+        if (err) {
+            console.error('[DB error]', err);
+            setTimeout(handleCon, 2000);
+        } else {
+            console.log('DB Connected!')
+        }
+    });
+
+    connection.on('error', err => {
+        console.error('[DB error]', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleCon();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleCon();
